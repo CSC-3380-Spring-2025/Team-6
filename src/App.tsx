32d5c4417085e35ce6f5adff43ec314304af3d0b
Ahmad Hijazi <ahmad.D.hijazi@gmail.com>
2025-03-21 15:35:09 -0500

    Setup Video Page For Section 1

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,11 @@ import * as React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import AuthPage from "./AuthPage";
 import HomePage from "./home-page";
+import VideoOne from "./video-one";
 
 const App: React.FC = () => {
   return (
+    /*/<VideoOne />*/
     <Router>
       <Routes>
         <Route path="/" element={<HomePage />} />
