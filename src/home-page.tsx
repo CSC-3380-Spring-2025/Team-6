76c310222c9924cc132a4a3c4e94d6f99f59ac5c
Ahmad Hijazi <ahmad.D.hijazi@gmail.com>
2025-04-07 15:17:10 -0500

    NavBar For Profile

--- src/home-page.tsx
+++ src/home-page.tsx
@@ -6,39 +6,74 @@ const HomePage: React.FC = () => {
 
   return (
     <div className="flex flex-col items-center min-h-screen bg-gray-100 p-4">
-      {/* Logo Section */}
-      <div className="flex justify-center items-center w-100 mt-1 mb-10">
-        <img src="CyberLogo.png" className="w-100 h-auto" alt="Cyber Logo" />
-        <div className="absolute top-0 right-0 flex m-4">
-          <button
-            className="btn btn-outline btn-error p-3"
-            onClick={() => navigate("/login")}
-          >
-            Login
-          </button>
-        </div>
-      </div>
-
       {/* Navigation Bar */}
-      <div className="flex justify-center items-center gap-6 mb-6">
-        <a
-          className="text-lg font-bold text-black hover:text-blue-600 transition"
-          href="url"
-        >
-          Home
-        </a>
-        <a
-          className="text-lg font-bold text-black hover:text-blue-600 transition"
-          href="url"
-        >
-          Courses
-        </a>
-        <a
-          className="text-lg font-bold text-black hover:text-blue-600 transition"
-          href="url"
-        >
-          About
-        </a>
+      <div className="navbar shadow-sm">
+        <div className="navbar-start">
+          <div className="dropdown">
+            <div
+              tabIndex={0}
+              role="button"
+              className="btn btn-ghost btn-circle"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                {" "}
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h16M4 18h7"
+                />{" "}
+              </svg>
+            </div>
+            <ul
+              tabIndex={0}
+              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
+            ></ul>
+          </div>
+        </div>
+        <div className="navbar-center">
+          {/* INSERT LOGO INSTEAD OF TEXT */}
+          <img src="CyberLogo.png" className="w-100 h-auto" alt="Cyber Logo" />
+        </div>
+        <div className="navbar-end">
+          <div className="dropdown dropdown-end">
+            <div
+              tabIndex={0}
+              role="button"
+              className="btn btn-ghost btn-circle avatar"
+            >
+              <div className="w-10 rounded-full">
+                <img
+                  alt="Tailwind CSS Navbar component"
+                  src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
+                />
+              </div>
+            </div>
+            <ul
+              tabIndex={0}
+              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
+            >
+              <li>
+                <a className="justify-between">
+                  Profile
+                  <span className="badge">New</span>
+                </a>
+              </li>
+              <li>
+                <a>Settings</a>
+              </li>
+              <li>
+                <a>Logout</a>
+              </li>
+            </ul>
+          </div>
+        </div>
       </div>
 
       {/* Section Buttons */}
