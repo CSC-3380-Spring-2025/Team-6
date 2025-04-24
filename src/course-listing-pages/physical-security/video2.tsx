b65608d744968e7a91f7bd6bc9acd0ffe7087a21
Thomas Doan <tdoan251@outlook.com>
2025-04-24 13:26:55 -0500

    Adjusted appearance of video pages

--- src/course-listing-pages/physical-security/video2.tsx
+++ src/course-listing-pages/physical-security/video2.tsx
@@ -5,21 +5,21 @@ const VideoTwo: React.FC = () => {
     const navigate = useNavigate();
 
   return (
-    <div data-theme="cybertheme" className="card bg-base-100 w-96 shadow-xl">
+    <div data-theme="cybertheme" className="card bg-base-100 w-full shadow-xl">
       <figure>
         <iframe
-          width="420"
-          height="315"
+          width="800vh"
+          height="500vh"
           src="https://www.youtube.com/embed/YtT8q2mUM9c?si=16H9Yr9gMtr6Cbfm"
         ></iframe>
       </figure>
       <div className="card-body">
-        <h2 className="card-title">Physical Security Video</h2>
+        <h2 className="card-title flex justify-center">Physical Security Video</h2>
         <p>
           Learn how Physical Security improves Cybersecurity
         </p>
         <div className="card-actions justify-end">
-        <button className="btn btn-outline btn-error p-3"
+        <button className="btn btn-error p-3 pointer-events-auto"
           onClick={() => navigate("/courselisting2")}>
             Finish
           </button>
