2613130c24495427bb746a35ca69421f266b721d
KyoukaHeiwa <bfacey3@lsu.edu>
2025-04-28 10:55:12 -0500

    Videos are looking fine now

--- src/course-listing-pages/phishing-and-social-engineering/video3.tsx
+++ src/course-listing-pages/phishing-and-social-engineering/video3.tsx
@@ -6,13 +6,15 @@ const VideoThree: React.FC = () => {
 
   return (
     <div data-theme="cybertheme" className="card bg-base-100 min-h-screen w-full shadow-xl">
-      <figure className="mt-10">
-        <iframe
-          width="800vh"
-          height="500vh"
-          src="https://www.youtube.com/embed/v7VTJhkJUUY?si=-KhGz9suIBaZUEHG"
-        ></iframe>
-      </figure>
+      <div className="bg-gray-700 rounded-lg justify-center mx-auto max-w-4xl">      
+        <figure className="m-10">
+          <iframe
+            width="800vh"
+            height="500vh"
+            src="https://www.youtube.com/embed/v7VTJhkJUUY?si=-KhGz9suIBaZUEHG"
+          ></iframe>
+        </figure>
+      </div>
       <div className="card-body text-center flex-none">
         <h2 className="card-title justify-center">Social Engineering Video</h2>
         <p>
--- src/course-listing-pages/physical-security/video2.tsx
+++ src/course-listing-pages/physical-security/video2.tsx
@@ -6,13 +6,15 @@ const VideoTwo: React.FC = () => {
 
   return (
     <div data-theme="cybertheme" className="card bg-base-100 min-h-screen w-full shadow-xl">
-      <figure className="mt-10">
-        <iframe
-          width="800vh"
-          height="500vh"
-          src="https://www.youtube.com/embed/YtT8q2mUM9c?si=16H9Yr9gMtr6Cbfm"
-        ></iframe>
-      </figure>
+      <div className="bg-gray-700 rounded-lg justify-center mx-auto max-w-4xl">      
+        <figure className="m-10">
+          <iframe
+            width="800vh"
+            height="500vh"
+            src="https://www.youtube.com/embed/YtT8q2mUM9c?si=16H9Yr9gMtr6Cbfm"
+          ></iframe>
+        </figure>
+      </div>
       <div className="card-body text-center flex-none">
         <h2 className="card-title justify-center">Physical Security Video</h2>
         <p>
