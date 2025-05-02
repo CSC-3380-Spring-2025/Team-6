7543e2ed33f3b95356a4dffd95187ded2fe3f195
KyoukaHeiwa <bfacey3@lsu.edu>
2025-05-02 14:10:46 -0500

    Redid video pages to better fit ipad resolution

--- src/course-listing-pages/password-security/video1.tsx
+++ src/course-listing-pages/password-security/video1.tsx
@@ -5,17 +5,15 @@ const VideoOne: React.FC = () => {
   const navigate = useNavigate();
 
   return (
-    <div data-theme="cybertheme" className="card bg-base-100 min-h-screen w-full shadow-xl">
+    <div data-theme="cybertheme" className="card bg-black min-h-screen w-full shadow-xl">
       <div className="bg-gray-700 rounded-lg justify-center mx-auto max-w-4xl">      
         <figure className="m-10">
-        <iframe
-          width="800vh"
-          height="500vh"
-          src="https://www.youtube.com/embed/xUp5S0nBnfc?si=KY0udN5Fz2wBWZ9x"
-        ></iframe>
-      </figure>
+          <iframe className="w-[720px] h-[450px] lg:w-[800px] lg:h-[500px] flex-none"
+            src="https://www.youtube.com/embed/xUp5S0nBnfc?si=KY0udN5Fz2wBWZ9x"
+          ></iframe>
+        </figure>
       </div>
-      <div className="card-body text-center flex-none">
+      <div className="max-w-4xl mt-10 mx-auto lg:m-auto rounded-lg items-center justify-center card-body text-center flex-none bg-base-100">
         <h2 className="card-title justify-center">Password Security Video</h2>
         <p>
           Learn the general tips to make a strong password and how to save your accounts.
--- src/course-listing-pages/phishing-and-social-engineering/video3.tsx
+++ src/course-listing-pages/phishing-and-social-engineering/video3.tsx
@@ -5,17 +5,15 @@ const VideoThree: React.FC = () => {
   const navigate = useNavigate();
 
   return (
-    <div data-theme="cybertheme" className="card bg-base-100 min-h-screen w-full shadow-xl">
+    <div data-theme="cybertheme" className="card bg-black min-h-screen w-full shadow-xl">
       <div className="bg-gray-700 rounded-lg justify-center mx-auto max-w-4xl">      
         <figure className="m-10">
-          <iframe
-            width="800vh"
-            height="500vh"
+          <iframe className="w-[720px] h-[450px] lg:w-[800px] lg:h-[500px] flex-none"
             src="https://www.youtube.com/embed/v7VTJhkJUUY?si=-KhGz9suIBaZUEHG"
           ></iframe>
         </figure>
       </div>
-      <div className="card-body text-center flex-none">
+      <div className="max-w-4xl mt-10 mx-auto lg:m-auto rounded-lg items-center justify-center card-body text-center flex-none bg-base-100">
         <h2 className="card-title justify-center">Social Engineering Video</h2>
         <p>
           Learn how people can be socially engineered into giving away important information.
